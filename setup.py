d5a1ac0e94d7f399749cd877725e54e59eeab25b
eadali <erkanadali91@gmail.com>
2024-09-26 13:48:42 +0300

    Convert repo to library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roborate',
+    version='0.1',
+    packages=find_packages(),
+    author='Erkan Adali',
+    author_email='erkanadali91@gmail.com.com',
+    description='A simple ROS Rate implementation with pure Python',
+    url='https://github.com/eadali/roborate',
+)
