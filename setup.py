539e8ae40b2bcc45d74fa7bd3712f522f1c5a2d9
Erkan <erkanadali91@gmail.com>
2024-09-26 14:21:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     author='Erkan Adali',
     author_email='erkanadali91@gmail.com.com',
     description='A simple ROS Rate implementation with pure Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/eadali/simple-rate',
 )
